98577c9731da9d92b3d3aadb8f6d16e01a5defb8
Tansu Daylan <tansu.daylan@gmail.com>
2024-07-07 22:16:10 -0500

    NA

--- examples/examples.py
+++ examples/examples.py
@@ -6,21 +6,40 @@ import chalcedon
 
 
 def cnfg_microlens():
+    numbsidecart = 100
+    maxmfovw = 2.
+    xposside = np.linspace(-maxmfovw, maxmfovw, 100)
+    yposside = xposside
+    xposgrid, yposgrid = np.meshgrid(xposside, yposside, indexing='ij')
+    xposgridflat = xposgrid.flatten()
+    yposgridflat = yposgrid.flatten()
+    
+    numbpixl = numbsidecart**2
+    indxpixl = np.arange(numbpixl)
     
     dictchalinpt = dict()
     dictchalinpt['sherextr'] = 0.3
     dictchalinpt['sangextr'] = 0.3
+    
+    dictchalinpt['xposhost'] = 0.3
+    dictchalinpt['yposhost'] = 0.3
+    dictchalinpt['beinhost'] = 0.3
+    dictchalinpt['ellphost'] = 0.3
     
     dictchalinpt['xpossubh'] = np.array([1.])
     dictchalinpt['ypossubh'] = np.array([1.])
     dictchalinpt['defssubh'] = np.array([1.])
     dictchalinpt['ascasubh'] = np.array([1.])
     dictchalinpt['acutsubh'] = np.array([1.])
+    
+    indxpixlelem = indxpixl
 
     dictchaloutp = chalcedon.retr_caustics( \
+                                           xposgrid=xposgridflat, \
+                                           yposgrid=yposgridflat, \
+                                           indxpixlelem=indxpixlelem, \
                                            dictchalinpt=dictchalinpt, \
                                           )
-    
 
 globals().get(sys.argv[1])(*sys.argv[2:])
 
